c58019bf7cd7f56b64531fc4e2804d32072af994
Ilya Korchenov <ilyakorchenov@mail.ru>
2015-12-30 19:39:27 +0300

    accordion and sliders complete

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,5 @@
 'use strict';
+
 function addPlaceholder() {
     if ($.fn.placeholder) {
         $('input, textarea').placeholder();
@@ -58,3 +59,48 @@ $('.clear-filter').on('click', function(e){
         .find('.sidebar-list__input[type="checkbox"]')
         .removeAttr('checked');
 });
+
+(function(){
+    var sliderBlock = $(".prise-slider"),
+        fromPrice= $('.sidebar-list__input-text_from'),
+        toPrice= $('.sidebar-list__input-text_to');
+
+    fromPrice.change(function () {
+        var val = $(this).val();
+        sliderBlock.slider("values",0,val);
+    });
+
+    toPrice.change( function() {
+        var val2 = $(this).val();
+        sliderBlock.slider("values",1,val2);
+    });
+
+    sliderBlock.slider({
+        min: 100,
+        max: 10000,
+        values: [100, 10000],
+        range: true,
+        step: 100,
+        slide: function( event, ui ) {
+            fromPrice.val(ui.values[0]);
+            toPrice.val(ui.values[1]);
+        }
+    });
+
+    fromPrice.val(sliderBlock.slider("values",0));
+    toPrice.val(sliderBlock.slider("values",1));
+})();
+
+$('.product-slider__min-image').on('click', function(){
+    var $this = $(this),
+        imgPath = $this.attr('src'),
+        mainImage = $this.closest('.product-slider').find('.product-slider__main-image');
+
+    mainImage.attr('src', imgPath);
+    $this.closest('.product-slider__min-image-item')
+        .addClass('product-slider__min-image-item_active')
+        .siblings()
+        .removeClass('product-slider__min-image-item_active')
+});
+
+$('.sort__select').select2();
